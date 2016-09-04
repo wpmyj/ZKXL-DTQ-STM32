708c16c5b762386097bedcdc25b52135ee36a2a1
samwu <1322673636@qq.com>
2016-09-04 15:19:17 +0800

    Project:Hex:rmmove nouse file
    Signed-off-by: samwu <1322673636@qq.com>

--- Hex/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\hex\core_cm3.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.c
-..\hex\core_cm3.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Hex/m24sr_cmd.d
+++
@@ -1,4 +0,0 @@
-..\hex\m24sr_cmd.o: ..\User\src\m24sr_cmd.c
-..\hex\m24sr_cmd.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-..\hex\m24sr_cmd.o: ..\User\inc\m24sr_cmd.h
-..\hex\m24sr_cmd.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
