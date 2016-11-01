1f0fdb1677137f6316603fcac5113b242d8c6bbb
Samwuzhitao <1322673636@qq.com>
2016-11-01 09:54:45 +0800

    Source:BUG:fix the bug of write uid pos to clicker
    Signed-off-by: Samwuzhitao <1322673636@qq.com>

--- User/app_send_data_process.h
+++ User/app_send_data_process.h
@@ -55,14 +55,14 @@
 /* 广播帧 */
 #define SEND_DATA1_STATUS           (SEND_PRE_STATUS+1)             // 2
 #define SEND_DATA1_SUM_TABLE        0
-#define SEND_DATA1_TIMEOUT          2050
+#define SEND_DATA1_TIMEOUT          4050
 #define SEND_DATA1_ACK_TABLE        3
 #define SEND_DATA1_UPDATE_STATUS    (SEND_DATA1_STATUS+1)           // 3
 
 /* 第1次重发帧 */
 #define SEND_DATA2_STATUS           (SEND_DATA1_UPDATE_STATUS+1)    // 4
 #define SEND_DATA2_SUM_TABLE        4
-#define SEND_DATA2_TIMEOUT          2000
+#define SEND_DATA2_TIMEOUT          4000
 #define SEND_DATA2_ACK_TABLE        5
 #define SEND_DATA2_SEND_OVER_STATUS (SEND_DATA2_STATUS+1)           // 5
 #define SEND_DATA2_UPDATE_STATUS    (SEND_DATA2_SEND_OVER_STATUS+1) // 6
@@ -70,7 +70,7 @@
 /* 第2次重发帧 */
 #define SEND_DATA3_STATUS           (SEND_DATA2_UPDATE_STATUS+1)    // 7
 #define SEND_DATA3_SUM_TABLE        6
-#define SEND_DATA3_TIMEOUT          2000
+#define SEND_DATA3_TIMEOUT          4000
 #define SEND_DATA3_ACK_TABLE        7
 #define SEND_DATA3_SEND_OVER_STATUS (SEND_DATA3_STATUS+1)           // 8
 #define SEND_DATA3_UPDATE_STATUS    (SEND_DATA3_SEND_OVER_STATUS+1) // 9
@@ -78,7 +78,7 @@
 /* 第3次重发帧 */
 #define SEND_DATA4_STATUS           (SEND_DATA3_UPDATE_STATUS+1)    // 10
 #define SEND_DATA4_SUM_TABLE        8
-#define SEND_DATA4_TIMEOUT          2000
+#define SEND_DATA4_TIMEOUT          4000
 #define SEND_DATA4_ACK_TABLE        9
 #define SEND_DATA4_UPDATE_STATUS    (SEND_DATA4_STATUS+1)           // 11
 
