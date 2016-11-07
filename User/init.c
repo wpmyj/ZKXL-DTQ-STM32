7e6ecf1f88cf60792971f27f91b094c584f9058d
Samwuzhitao <1322673636@qq.com>
2016-11-07 16:36:29 +0800

    Source:Code:rm nouse file
    Signed-off-by: Samwuzhitao <1322673636@qq.com>

--- User/init.c
+++ User/init.c
@@ -10,10 +10,14 @@
   
 #include "main.h"
 #include "mcu_config.h"
+#include "nrf.h"
+/* Private variables ---------------------------------------------------------*/
+spi_cmd_type_t 					 spi_cmd_type;
+nrf_communication_t			 nrf_communication;
 extern WhiteList_Typedef wl;
 extern nrf_communication_t nrf_communication;
 /*******************************************************************************
-  * @brief  Ó²¼şÆ½Ì¨³õÊ¼»¯
+  * @brief  ç¡¬ä»¶å¹³å°åˆå§‹åŒ–
   * @param  None
   * @retval None
   * @note 	None		  
@@ -37,7 +41,7 @@ void Platform_Init(void)
 	/* get mcu uuid */
 	get_mcu_uid();
 	
-	/* Åä¶ÔÊÇ´æÈë½ÓÊÕÆ÷Æ÷UIDµ½´ğÌâÆ÷ */
+	/* é…å¯¹æ˜¯å­˜å…¥æ¥æ”¶å™¨å™¨UIDåˆ°ç­”é¢˜å™¨ */
 	NDEF_DataWrite[1] = 0x06;
 	memcpy(NDEF_DataWrite+2,nrf_communication.jsq_uid,4);;
 	
@@ -50,14 +54,14 @@ void Platform_Init(void)
 	DebugLog("\r\n[%s]:White list len = %d \r\n",__func__, wl.len);
 	DebugLog("[%s]:White list switch status is %d \r\n",__func__, wl.switch_status);
 	
-	/* ¸´Î»²¢³õÊ¼»¯RC500 */
+	/* å¤ä½å¹¶åˆå§‹åŒ–RC500 */
 	GPIOInit_MFRC500();
 	temp = PcdReset();															
 	
 	/* enable all IRQ */
 	ENABLE_ALL_IRQ();
 	
-	/* led ¡¢·äÃùÆ÷ÉùÒôÌáÊ¾³õÊ¼»¯Íê±Ï */
+	/* led ã€èœ‚é¸£å™¨å£°éŸ³æç¤ºåˆå§‹åŒ–å®Œæ¯• */
 	//BEEP_EN();																	    
 	ledOn(LGREEN);																 
 	ledOn(LBLUE);																    
@@ -77,7 +81,7 @@ void Platform_Init(void)
 	}
 	else
 	{
-		/* ³õÊ¼»¯ºó¹Ø±ÕÌìÏß */
+		/* åˆå§‹åŒ–åå…³é—­å¤©çº¿ */
 		PcdAntennaOff();
 		DebugLog("[%s]:MFRC 500 reset ok\r\n",__func__);
 	}
@@ -91,12 +95,12 @@ void Platform_Init(void)
 }
 
 /****************************************************************************
-* Ãû    ³Æ£ºvoid Usart1_Init(void)
-* ¹¦    ÄÜ£º´®¿Ú1³õÊ¼»¯º¯Êı
-* Èë¿Ú²ÎÊı£ºÎŞ
-* ³ö¿Ú²ÎÊı£ºÎŞ
-* Ëµ    Ã÷£º
-* µ÷ÓÃ·½·¨£ºÎŞ
+* å    ç§°ï¼švoid Usart1_Init(void)
+* åŠŸ    èƒ½ï¼šä¸²å£1åˆå§‹åŒ–å‡½æ•°
+* å…¥å£å‚æ•°ï¼šæ— 
+* å‡ºå£å‚æ•°ï¼šæ— 
+* è¯´    æ˜ï¼š
+* è°ƒç”¨æ–¹æ³•ï¼šæ— 
 ****************************************************************************/
 void Usart1_Init(void)
 {
@@ -132,7 +136,7 @@ void Usart1_Init(void)
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	//ÖĞ¶ÏÅäÖÃ..Only IDLE Interrupt..
+	//ä¸­æ–­é…ç½®..Only IDLE Interrupt..
 	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
 	
 	/* Enable the USART1 */
@@ -140,12 +144,12 @@ void Usart1_Init(void)
 }
 
 /****************************************************************************
-* Ãû    ³Æ£ºvoid Usart2_Init(void)
-* ¹¦    ÄÜ£º´®¿Ú2³õÊ¼»¯º¯Êı
-* Èë¿Ú²ÎÊı£ºÎŞ
-* ³ö¿Ú²ÎÊı£ºÎŞ
-* Ëµ    Ã÷£º
-* µ÷ÓÃ·½·¨£ºÎŞ
+* å    ç§°ï¼švoid Usart2_Init(void)
+* åŠŸ    èƒ½ï¼šä¸²å£2åˆå§‹åŒ–å‡½æ•°
+* å…¥å£å‚æ•°ï¼šæ— 
+* å‡ºå£å‚æ•°ï¼šæ— 
+* è¯´    æ˜ï¼š
+* è°ƒç”¨æ–¹æ³•ï¼šæ— 
 ****************************************************************************/
 void Usart2_Init(void)
 {
@@ -181,7 +185,7 @@ void Usart2_Init(void)
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	//ÖĞ¶ÏÅäÖÃ..Only IDLE Interrupt..
+	//ä¸­æ–­é…ç½®..Only IDLE Interrupt..
 	USART_ITConfig(USART2,USART_IT_RXNE,ENABLE);
 	
 	/* Enable the USART1 */
@@ -200,7 +204,7 @@ void uart_send_char( uint8_t ch )
 }
 /*******************************************************************************
 * Function Name   : int fputc(int ch, FILE *f)
-* Description     : Retargets the C library printf function to the USART.printfÖØ¶¨Ïò
+* Description     : Retargets the C library printf function to the USART.printfé‡å®šå‘
 * Input           : None
 * Output          : None
 * Return          : None
@@ -220,10 +224,10 @@ int fputc(int ch, FILE *f)
 
 /*******************************************************************************
 * Function Name   : int fgetc(FILE *f)
-* Description     : Retargets the C library printf function to the USART.fgetcÖØ¶¨Ïò
+* Description     : Retargets the C library printf function to the USART.fgetcé‡å®šå‘
 * Input           : None
 * Output          : None
-* Return          : ¶ÁÈ¡µ½µÄ×Ö·û
+* Return          : è¯»å–åˆ°çš„å­—ç¬¦
 *******************************************************************************/
 int fgetc(FILE *f)
 {
@@ -325,5 +329,115 @@ void GPIOInit_MFRC500(void)
     GPIO_WriteBit(MFRC500_DATA_Port, MFRC500_DATA_Pin, Bit_RESET);
 }
 
+
+/* Private functions ---------------------------------------------------------*/
+static uint8_t hal_nrf_rw(SPI_TypeDef* SPIx, uint8_t value)
+{
+	while(SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_TXE) == RESET);
+	SPI_I2S_SendData(SPIx, value);
+	while(SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_RXNE) == RESET);
+	return(SPI_I2S_ReceiveData(SPIx));
+}
+
+uint8_t uesb_nrf_get_irq_flags(SPI_TypeDef* SPIx, uint8_t *flags, uint8_t *rx_data_len, uint8_t *rx_data)
+{
+	uint8_t retval[BUFFER_SIZE_MAX];
+	uint8_t i = 0;
+	uint8_t *temp_data = NULL;
+
+	*rx_data_len = 0;
+	memset(spi_cmd_type.data, 0xFF, BUFFER_SIZE_MAX);
+	spi_cmd_type.spi_cmd = UESB_READ_RF_INT_STATUS;
+	spi_cmd_type.data_len = 0x02;
+	spi_cmd_type.data[0] = 0xFF;
+	spi_cmd_type.data[1] = 0xFF;
+	temp_data = (uint8_t *)&spi_cmd_type;
+
+	CSN_LOW();	//å¼€å§‹SPIä¼ è¾“
+	memset(retval, 0, BUFFER_SIZE_MAX);
+	for(i=0; i<spi_cmd_type.data_len+3; i++)
+	{
+		retval[i] = hal_nrf_rw(SPIx, *(temp_data+i));
+		//printf("%2x ",retval[i]);
+		if(i ==  2 && retval[2] != 0x00 && retval[2] != 0xFF)
+		{
+			*flags = retval[2];
+		}
+
+		if( i == 3 && (retval[2] & (1<<RX_DR)) && retval[3] < BUFFER_SIZE_MAX )				// If "received data ready" interrupt from hrf
+	  {
+			*rx_data_len = retval[3];
+			spi_cmd_type.data_len += *rx_data_len;
+			spi_cmd_type.data[spi_cmd_type.data_len] = XOR_Cal((uint8_t *)&spi_cmd_type.data[3], spi_cmd_type.data_len - 3);
+		}
+	}
+	//printf("\r\n");
+	CSN_HIGH();	//å…³é—­SPIä¼ è¾“
+
+	memcpy(rx_data, &retval[4],*rx_data_len);
+	//DELAY_FUNC(DELAY_TIME);
+
+	if(retval[0] != 0 && retval[0] != 0xFF) 			//è‹¥æ¥æ”¶åˆ°æ•°æ®æ ¡éªŒæ­£ç¡®
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+uint8_t uesb_nrf_write_tx_payload(const uint8_t *tx_pload, uint8_t length, uint8_t count, uint8_t delay100us)
+{
+	uint8_t retval[BUFFER_SIZE_MAX];
+	uint16_t i = 0;
+	uint8_t *temp_data = NULL;
+
+	spi_cmd_type.spi_cmd = UESB_WRITE_TX_PAYLOAD;
+	spi_cmd_type.data_len = length+2;
+	spi_cmd_type.count = count;
+	spi_cmd_type.delay100us = delay100us;
+
+	memcpy(spi_cmd_type.data, tx_pload, length);
+	spi_cmd_type.data[spi_cmd_type.data_len-2] = XOR_Cal((uint8_t *)&spi_cmd_type, spi_cmd_type.data_len+2);
+	temp_data = (uint8_t *)&spi_cmd_type;
+
+	CSN_LOW_2();	//å¼€å§‹SPIä¼ è¾“
+	memset(retval, 0, BUFFER_SIZE_MAX);
+	for(i=0; i<spi_cmd_type.data_len+3; i++)
+	{
+		retval[i] = hal_nrf_rw(SPI1, *(temp_data+i));
+	}
+	CSN_HIGH_2();	//å…³é—­SPIä¼ è¾“
+
+	if(retval[0] != 0) 									//è‹¥æ¥æ”¶åˆ°æ•°æ®æ ¡éªŒæ­£ç¡®
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*********************************************************************************
+**åŠŸ	èƒ½ï¼šå¼‚æˆ–è®¡ç®—å‡½æ•°
+**è¾“    å…¥: data	å¾…è®¡ç®—çš„æ•°æ®å—
+**		  : length	æ•°æ®å—é•¿åº¦
+**è¿”	å›ï¼šå¼‚æˆ–è®¡ç®—ç»“æœ
+**å¤‡	æ³¨ï¼šNULL
+*********************************************************************************/
+uint8_t XOR_Cal(uint8_t *data, uint16_t length)
+{
+	uint8_t temp_xor;
+	uint16_t i;
+
+	temp_xor = *data;
+	for(i = 1;i < length; i++)
+	{
+		temp_xor = temp_xor ^ *(data+i);
+	}
+	return temp_xor;
+}
 /**************************************END OF FILE****************************/
 
