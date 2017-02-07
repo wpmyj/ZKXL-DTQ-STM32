94e9e80bab72f38f62bd18032c72d1284503b16e
samwu <1322673636@qq.com>
2017-02-07 13:07:52 +0800

    Source:BUG:允许读出错误的数据，否则会进入逻辑死循环。

--- User/app_card_process.c
+++ User/app_card_process.c
@@ -281,8 +281,9 @@ void App_card_process(void)
 					DEBUG_CARD_DATA_LOG("\r\n");
 					ndef_rd_xor        = XOR_Cal(NDEF_DataRead+1,26);
 					if((NDEF_DataRead[6]  != NDEF_DataWrite[6])  || 
-						 (NDEF_DataRead[27] != NDEF_DataWrite[27]) || 
-					   (NDEF_DataRead[27] != ndef_rd_xor))
+						 (NDEF_DataRead[27] != NDEF_DataWrite[27]) )  
+					 /* 此处一定要允许读出错误的数据，否则会进入逻辑的死循环 */
+					 //(NDEF_DataRead[27] != ndef_rd_xor)) 
 					{
 						/* 需要重新写入数据 */
 					}
