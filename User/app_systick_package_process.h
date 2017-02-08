4e54e02e31e64b51c64350fb93ce813fbed81b5c
samwu <1322673636@qq.com>
2017-02-08 12:09:02 +0800

    Source:Code:修改心跳逻辑为PC发个接收器时，接收器再转发个答题器      时间：2017-02-08      描述：[1].加上心跳逻辑，修改心跳逻辑为PC发个接收器时，接收器再转发个答题器

--- User/app_systick_package_process.h
+++ User/app_systick_package_process.h
@@ -10,7 +10,6 @@ void rf_change_systick_status(uint8_t rf_status);
 uint8_t rf_get_systick_status(void);
 void App_clickers_systick_process(void);
 void systick_package_timer_init( void );
-uint8_t systick_get_ack_funcction_para( void );
 void systick_set_ack_funcction( uint8_t open_or_close );
 
 #endif
